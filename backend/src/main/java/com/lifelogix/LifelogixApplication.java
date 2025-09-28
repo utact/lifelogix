3b292f363ba7c60ffe4517a2867b95ef7539d1ba
Yoo Seung-jun <funczun@gmail.com>
2025-09-28 16:43:18 +0900

    feat(backend): Initialize Spring Boot project structure

---
+++ backend/src/main/java/com/lifelogix/LifelogixApplication.java
@@ -0,0 +1,13 @@
+package com.lifelogix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LifelogixApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LifelogixApplication.class, args);
+	}
+
+}
