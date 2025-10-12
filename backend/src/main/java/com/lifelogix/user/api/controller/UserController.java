42d08c59e1f416bcaa806f0b5b90f139fe929559
Yoo Seung-jun <funczun@gmail.com>
2025-10-12 17:02:52 +0900

    refactor(user): Use Principal to retrieve authenticated user

--- backend/src/main/java/com/lifelogix/user/api/controller/UserController.java
+++ backend/src/main/java/com/lifelogix/user/api/controller/UserController.java
@@ -5,10 +5,11 @@ import com.lifelogix.user.application.UserService;
 import com.lifelogix.user.domain.User;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.security.Principal;
 
 @RestController
 @RequestMapping("/api/v1/users")
@@ -21,7 +22,8 @@ public class UserController {
      * 현재 인증된 사용자의 정보(내 정보)를 조회
      **/
     @GetMapping("/me")
-    public ResponseEntity<UserResponse> getMyInfo(@AuthenticationPrincipal Long userId) {
+    public ResponseEntity<UserResponse> getMyInfo(Principal principal) {
+        Long userId = Long.parseLong(principal.getName());
         User user = userService.getUserById(userId);
         return ResponseEntity.ok(UserResponse.from(user));
     }
