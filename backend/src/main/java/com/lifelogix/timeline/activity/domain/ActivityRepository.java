a3ab1145c140f5d688bd6d5d910d4b2079fb0b95
Yoo Seungjun <funczun@gmail.com>
2025-10-15 16:43:40 +0900

    fix(backend): Resolve LazyInitializationException with fetch joins

--- backend/src/main/java/com/lifelogix/timeline/activity/domain/ActivityRepository.java
+++ backend/src/main/java/com/lifelogix/timeline/activity/domain/ActivityRepository.java
@@ -8,7 +8,7 @@ import org.springframework.data.repository.query.Param;
 import java.util.List;
 
 public interface ActivityRepository extends JpaRepository<Activity, Long> {
-    @Query("SELECT a FROM Activity a JOIN FETCH a.category c LEFT JOIN FETCH c.user WHERE a.user.id = :userId ORDER BY c.name, a.name")
+    @Query("SELECT a FROM Activity a JOIN FETCH a.user JOIN FETCH a.category c LEFT JOIN FETCH c.user WHERE a.user.id = :userId ORDER BY c.name, a.name")
     List<Activity> findByUserIdOrderByCategory(@Param("userId") Long userId);
     boolean existsByCategory(Category category);
     boolean existsByCategoryAndName(Category category, String name);
