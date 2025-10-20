0763ed47ec96776f60f2aa45516d6136e6b3f115
unknown <Yoo Seung-jun>
2025-10-20 16:20:47 +0900

    feat(common): Health Check API가 동적 빌드 정보를 반환하도록 수정

--- backend/src/main/java/com/lifelogix/common/api/controller/HealthCheckController.java
+++ backend/src/main/java/com/lifelogix/common/api/controller/HealthCheckController.java
@@ -1,12 +1,19 @@
 package com.lifelogix.common.api.controller;
 
 import com.lifelogix.common.api.dto.response.HealthCheckResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.info.BuildProperties;
+import org.springframework.boot.info.GitProperties;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequiredArgsConstructor
 public class HealthCheckController {
+
+    private final BuildProperties buildProperties;
+    private final GitProperties gitProperties;
 
     /**
      * 애플리케이션의 상태, 버전, 커밋 해시를 반환하여 헬스 체크를 수행
@@ -14,8 +21,7 @@ public class HealthCheckController {
      */
     @GetMapping("/api/v1/health")
     public ResponseEntity<HealthCheckResponse> healthCheck() {
-        // TODO: 빌드 시 주입되는 프로퍼티 값을 읽어오도록 구성
-        HealthCheckResponse response = new HealthCheckResponse("UP", "1.0.0-logging", "f1f1900");
+        HealthCheckResponse response = new HealthCheckResponse("UP", buildProperties.getVersion(), gitProperties.getShortCommitId());
         return ResponseEntity.ok(response);
     }
 }
