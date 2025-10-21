1b2cb77f500f991f48b955ad0002ab169db6d90c
Yoo Seung-jun <funczun@gmail.com>
2025-10-21 14:04:04 +0900

    fix(BE): 헬스 체크가 더이상 커밋 해시를 받지 않도록 수정

--- backend/src/main/java/com/lifelogix/common/api/controller/HealthCheckController.java
+++ backend/src/main/java/com/lifelogix/common/api/controller/HealthCheckController.java
@@ -3,7 +3,6 @@ package com.lifelogix.common.api.controller;
 import com.lifelogix.common.api.dto.response.HealthCheckResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.info.BuildProperties;
-import org.springframework.boot.info.GitProperties;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,15 +12,19 @@ import org.springframework.web.bind.annotation.RestController;
 public class HealthCheckController {
 
     private final BuildProperties buildProperties;
-    private final GitProperties gitProperties;
 
     /**
-     * 애플리케이션의 상태, 버전, 커밋 해시를 반환하여 헬스 체크를 수행
+     * 애플리케이션의 상태, 버전을 반환하여 헬스 체크를 수행 (커밋 해시 제외)
      * @return HealthCheckResponse DTO를 포함하는 ResponseEntity
      */
     @GetMapping("/api/v1/health")
     public ResponseEntity<HealthCheckResponse> healthCheck() {
-        HealthCheckResponse response = new HealthCheckResponse("UP", buildProperties.getVersion(), gitProperties.getShortCommitId());
+        // GitProperties 대신 빈 문자열("") 사용
+        HealthCheckResponse response = new HealthCheckResponse(
+                "UP",
+                buildProperties.getVersion(),
+                "" // gitProperties.getShortCommitId() 대신 빈 문자열
+        );
         return ResponseEntity.ok(response);
     }
 }
